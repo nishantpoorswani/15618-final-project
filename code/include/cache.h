0926ee3d5a9c8a655fa75af666d39ad62deb0725
ShridharG <sganiger@andrew.cmu.edu>
2022-04-09 19:23:48 -0400

    Improved style

--- code/include/cache.h
+++ code/include/cache.h
@@ -8,79 +8,83 @@
 #include <queue>
 #include <list>
 
+namespace cacheSim
+{
+    class cache 
+    {
+        protected:
+            int hits;
+            int misses;
+            int evictions;
+            int dirty_bytes;
+            int dirty_evictions;
+        public:
+            struct cacheLine {
+                long addr;  //add based on usage
+                long tag;
+                bool valid;
+                bool dirty;
+                cacheLine(long addr, long tag):
+                    addr(addr), tag(tag), valid(true), dirty(false) {}
+                cacheLine(long addr, long tag, bool valid, bool dirty):
+                    addr(addr), tag(tag), valid(valid), dirty(dirty) {}
+            };
 
-class cache {
+            cache(int sets, int associativity, int blockSize);
 
-protected:
-    hits;
-    misses;
-    evictions;
-    dirty_bytes;
-    dirty_evictions;
-public:
-    struct cacheLine {
-        long addr;  //add based on usage
-        long tag;
-        bool valid;
-        bool dirty;
-        Line(long addr, long tag):
-            addr(addr), tag(tag), lock(true), dirty(false) {}
-        Line(long addr, long tag, bool lock, bool dirty):
-            addr(addr), tag(tag), lock(lock), dirty(dirty) {}
+            void cacheLogic(char operation, long address);
+
+        protected:
+            unsigned int sets;
+            unsigned int associativity;
+            unsigned int blockNum;
+            unsigned int indexMask;
+            unsigned int blockSize;
+            unsigned int indexOffset;
+            unsigned int tagOffset;
+
+            std::map<int, std::list<cacheLine> > cacheSet;
+
+            int calcLog2(int val) 
+            {
+                int n = 0;
+                while ((val >>= 1))
+                    n ++;
+                return n;
+            }
+
+            int getIndex(long addr) 
+            {
+                return (addr >> indexOffset) & indexMask;
+            };
+
+            long getTag(long addr) 
+            {
+                return (addr >> tagOffset);
+            }
+
+            long align(long addr) 
+            {
+                return (addr & ~(blockSize-1l));
+            }
+
+            void evict(std::list<cacheLine>* line, std::list<cacheLine>::iterator victim);
+
+            void allocateLine(std::list<cacheLine>& lines, long addr, int coreid);
+
+            bool needEviction(const std::list<cacheLine>& lines, long addr);
+            
+            bool isHit(std::list<cacheLine>& line, long addr, std::list<cacheLine>::iterator* posPtr);
+
+            std::list<cacheLine>& getLine(long addr) 
+            {
+                if (cacheSet.find(getIndex(addr))
+                    == cacheSet.end()) 
+                {
+                    cacheSet.insert(make_pair(getIndex(addr), std::list<cacheLine>()));
+                }
+                return cacheSet[getIndex(addr)];
+            }
+
     };
-
-    cache(int sets, int associativity, int blockSize);
-
-protected:
-    unsigned int sets;
-    unsigned int associativity;
-    unsigned int blockNum;
-    unsigned int indexMask;
-    unsigned int blockSize;
-    unsigned int indexOffset;
-    unsigned int tagOffset;
-
-    std::map<int, std::list<cacheLine> > cacheSet;
-
-    int calcLog2(int val) {
-        int n = 0;
-        while ((val >>= 1))
-            n ++;
-        return n;
-    }
-
-    int getIndex(long addr) {
-        return (addr >> indexOffset) & indexMask;
-    };
-
-    long getTag(long addr) {
-        return (addr >> tagOffset);
-    }
-
-    long align(long addr) {
-        return (addr & ~(blockSize-1l));
-    }
-
-    void evictline(long addr, bool dirty);
-
-    void evict(std::list<cacheLine>* line,
-      std::list<cacheLine>::iterator victim);
-
-    std::list<cacheLine>::iterator allocateLine(
-      std::list<cacheLine>& line, long addr);
-
-    bool need_eviction(const std::list<cacheLine>& lines, long addr);
-
-    bool isHit(std::list<cacheLine>& line, long addr,
-              std::list<cacheLine>::iterator* posPtr);
-
-    std::list<cacheLine>& getLine(long addr) {
-    if (cacheSet.find(getIndex(addr))
-        == cacheSet.end()) {
-        cacheSet.insert(make_pair(getIndex(addr),
-            std::list<cacheLine>()));
-    }
-    return cacheSet[getIndex(addr)];
-  }
-
-};
+} // namespace cacheSim
--- code/src/cache.cpp
+++ code/src/cache.cpp
@@ -15,14 +15,12 @@
 
 namespace cacheSim
 {
-
-cache::cache(int sets, int associativity, int blockSize):
-    sets(sets), associativity(associativity),
-    blockSize(blockSize){
-
+  cache::cache(int sets, int associativity, int blockSize):
+      sets(sets), associativity(associativity),
+      blockSize(blockSize)
+  {
     debug("sets %d associativity %d blockSize %d\n",
         sets, associativity, blockSize);
-
 
     int size = sets*associativity*blockSize;
 
@@ -47,91 +45,68 @@ cache::cache(int sets, int associativity, int blockSize):
     evictions = 0;
     dirty_bytes = 0;
     dirty_evictions = 0;
-}
+  }
 
-void cache::evict(std::list<cacheLine>* lines,
-    std::list<cacheLine>::iterator victim) {
-  debug("miss evict victim %lx", victim->addr);
-  evictions++;
-  lines->erase(victim);
-}
+  void cache::evict(std::list<cacheLine>* lines,
+      std::list<cacheLine>::iterator victim) 
+  {
+    debug("miss evict victim %lx", victim->addr);
+    evictions++;
+    lines->erase(victim);
+  }
 
-bool cache::needEviction(const std::list<cacheLine>& lines, long addr) {
-  // if (find_if(lines.begin(), lines.end(),
-  //     [addr, this](cacheLine l){
-  //       return (getTag(addr) == l.tag);})
-  //     != lines.end()) {
-  //   assert(false);
-  // } else {
-    if (lines.size() < associativity) {
-      return false;
-    } else {
-      return true;
+  bool cache::needEviction(const std::list<cacheLine>& lines, long addr) 
+  {
+    return (lines.size() < associativity ? false : true); 
+  }
+
+  void cache::allocateLine(std::list<cacheLine>& lines, long addr, int coreid) 
+  {
+    if (needEviction(lines, addr)) 
+    {
+      // Get victim.
+      auto victim =lines.begin();
+      assert(victim != lines.end());
+      evict(&lines, victim);
     }
-  //}
-}
+    // Allocate newline, with dirty bit off
+    lines.push_back(cacheLine(addr, getTag(addr)));
+  }
 
-std::list<cache::cacheLine>::iterator cache::allocateLine(
-    std::list<cacheLine>& lines, long addr, int coreid) 
-{
-  if (needEviction(lines, addr)) 
+  bool cache::isHit(std::list<cacheLine>& lines, long addr,
+      std::list<cacheLine>::iterator* pos_ptr) 
   {
-    // Get victim.
-    // The first one might still be locked due to reorder in MC
-    auto victim =lines.begin();
-    assert(victim != lines.end());
-    evict(&lines, victim);
+    auto pos = find_if(lines.begin(), lines.end(),
+        [addr, this](cacheLine l){return (l.tag == getTag(addr));});
+    *pos_ptr = pos;
+    if (pos == lines.end()) {
+      return false;
+    }
+    return true;
   }
 
-  // Allocate newline, with lock bit on and dirty bit off
-  lines.push_back(cacheLine(addr, getTag(addr)));
-  auto last_element = lines.end();
-  --last_element;
-  return last_element;
-}
-
-bool cache::isHit(std::list<cacheLine>& lines, long addr,
-    std::list<cacheLine>::iterator* pos_ptr) {
-  auto pos = find_if(lines.begin(), lines.end(),
-      [addr, this](cacheLine l){return (l.tag == getTag(addr));});
-  *pos_ptr = pos;
-  if (pos == lines.end()) {
-    return false;
-  }
-  return true;
-}
-
-void cache::cacheLogic(char operation, long address)
-{
-
-  debug("address %lx type %d, index %d, tag %ld",
-        address, operation, getIndex(address),
-      getTag(address));
-
-  // If there isn't a set, create it.
-  auto& caSet = getLine(address);
-  std::list<cacheLine>::iterator line;
-
-  if (isHit(caSet, address, &line)) 
+  void cache::cacheLogic(char operation, long address)
   {
-    caSet.push_back(cacheLine(address, getTag(address), false,
-        line->dirty || (operation == 'S')));
-    caSet.erase(line);
-    hits++;
-    //return true;
-  }
-  else 
-  {
-    misses++;
-    // The dirty bit will be set if this is a write request and @L1
-    //bool dirty = (operation == 'S');
-    //auto newline = allocateLine(caSet, address, 0);
-    allocateLine(caSet, address, 0);
-    // if (newline == caSet.end()) {
-    //   return false;
-    // }
-  }
-  std::cout << "hits:" << hits << " misses:" << misses << " evictions" << evictions << "\n";
-}
+    debug("address %lx type %d, index %d, tag %ld",
+          address, operation, getIndex(address),
+        getTag(address));
 
+    // If there isn't a set, create it.
+    auto& caSet = getLine(address);
+    std::list<cacheLine>::iterator line;
+
+    if (isHit(caSet, address, &line)) 
+    {
+      caSet.push_back(cacheLine(address, getTag(address), false,
+          line->dirty || (operation == 'S')));
+      caSet.erase(line);
+      hits++;
+    }
+    else 
+    {
+      misses++;
+      allocateLine(caSet, address, 0);
+    }
+    std::cout << "hits:" << hits << " misses:" << misses << " evictions" << evictions << "\n";
+  }
 } // namespace cacheSim
