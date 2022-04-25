82825e3e64331b86a73332b79a85f0566b0d8f97
ShridharG <sganiger@andrew.cmu.edu>
2022-04-25 15:59:34 -0400

    Modified MI: to properly consider bus actions

--- code/include/MI.h
+++ code/include/MI.h
@@ -27,8 +27,9 @@ namespace cacheSim
 
             enum busAction
             {
-                busRd = 0,
-                busWr = 1
+                noBusAction = 0,
+                busRd = 1,
+                busWr = 2
             };
 
             enum prAction
@@ -39,9 +40,9 @@ namespace cacheSim
 
         void controller(int numCores, cache** cacheCore, int tid, char operation, long address);
 
-        void handleProcessorAction(cacheSim::cache **cacheCore, int tid, char operation, long address);
+        busAction handleProcessorAction(cacheSim::cache **cacheCore, int tid, char operation, long address, prAction prAc);
 
-        void handleBusAction(int numCores, cacheSim::cache **cacheCore, int tid,prAction prAc, long address);
+        void handleBusAction(int numCores, cacheSim::cache **cacheCore, int tid,prAction prAc, long address, busAction busAc);
     };
 }
 
--- code/src/MI.cpp
+++ code/src/MI.cpp
@@ -15,59 +15,92 @@
 namespace cacheSim
 {
 
-    void MI::handleProcessorAction(cacheSim::cache **cacheCore, int tid, char operation, long address)
+    MI::busAction MI::handleProcessorAction(cacheSim::cache **cacheCore, int tid, char operation, long address, prAction prAc)
     {
+        lineState prevLineState;
+        busAction busAc = noBusAction;
+
         /* Update cache for the requested memory event for a particular cache */
         cacheCore[tid]->cacheLogic(operation, (long)address);
 
         /* Update the state of the cache line to modified */
         auto& caSet = cacheCore[tid]->getLine(address);
         std::list<cacheSim::cache::cacheLine>::iterator line = caSet.end();
-        //std::cout << "Inside processor action after end size: " << caSet.size() << "\n";
         line--;
-        //std::cout << "Inside processor action after decrement iterator: address: " << line->addr << " " << line->tag << " " << line->state << "\n";
-        line->state = (int)modified;
+
+        /* Get the previous line state */
+        prevLineState = (lineState) line->state;
+
+        if(prAc == prRd)
+        {
+            if (prevLineState == invalid)
+            {
+                busAc = busRd;
+                line->state = (int)modified;
+            }
+            else
+            {
+                /*Line is expected to be in modified state */
+                /* Can add an assert here to check above */
+            }
+        }
+        if(prAc == prWr)
+        {
+            if (prevLineState == invalid)
+            {
+                busAc = busWr;
+                line->state = (int)modified;
+            }
+            else
+            {
+                /*Line is expected to be in modified state */
+                /* Can add an assert here to check above */
+            }    
+        }
+
+        return busAc;
         //std::cout << "Inside processor action after change line state: address: " << line->addr << " " << line->tag << " " << line->state << "\n";
     }
 
-    void MI::handleBusAction(int numCores, cacheSim::cache **cacheCore, int tid,prAction prAc, long address)
+    void MI::handleBusAction(int numCores, cacheSim::cache **cacheCore, int tid,prAction prAc, long address, busAction busAc)
     {
-        /* Based on the processor action, generate a bus action 
-        and update the cache line state in other processor caches */
-        busAction busAc;
-        busAc = (prAc == prRd) ? busRd : busWr; 
 
-        if(busAc == busWr)
+        for(int i=0; i < numCores ; i++)
         {
-            for(int i=0; i < numCores ; i++)
+            if(i != tid)
             {
-                if(i != tid)
+                /* Update the state of the cache line in other caches to invalid */
+                auto& caSet = cacheCore[i]->getLine(address);
+                auto line = find_if(caSet.begin(), caSet.end(),
+                    [address, cacheCore, i](cacheSim::cache::cacheLine l){return (l.tag == cacheCore[i]->getTag(address));});
+                if (line == caSet.end()) {
+                    continue;
+                }
+                //std::cout << "Inside bus action: address: " << address << " tid: " << tid << "\n"; 
+                if(busAc == busWr)
                 {
-                    /* Update the state of the cache line in other caches to invalid */
-                    auto& caSet = cacheCore[i]->getLine(address);
-                    auto line = find_if(caSet.begin(), caSet.end(),
-                        [address, cacheCore, i](cacheSim::cache::cacheLine l){return (l.tag == cacheCore[i]->getTag(address));});
-                    if (line == caSet.end()) {
-                        continue;
-                    }
-                    //std::cout << "Inside bus action: address: " << address << " tid: " << tid << "\n"; 
                     line->state = (int)invalid;
                     caSet.erase(line);
                 }
+                else
+                {
+                    /* Need not change the line state if it's a busRd */
+                }
             }
-        }   
+        } 
     }
 
     void MI::controller(int numCores, cacheSim::cache **cacheCore, int tid, char operation, long address)
     {
         //std::cout << "address: " << address << " tid: " << tid << "\n";
-        prAction prAc; 
-        
-        handleProcessorAction(cacheCore, tid, operation, address);
+        prAction prAc;  //processor action
+        busAction busAc = noBusAction; //bus action
 
         prAc = (operation == 'L') ? prRd : prWr;
+        
+        busAc = handleProcessorAction(cacheCore, tid, operation, address, prAc);
 
-        handleBusAction(numCores, cacheCore, tid, prAc, address);
+        handleBusAction(numCores, cacheCore, tid, prAc, address, busAc);
 
         //cacheCore[0]->cacheLogic(operation, (long)address);
     }
