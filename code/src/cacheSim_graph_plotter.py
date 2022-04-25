54a384a2ecadd7deb9b4d3e4875c9978942e8333
Nishant Poorswani <npoorswa@andrew.cmu.edu>
2022-04-25 18:08:25 -0400

    Fixed graph labels
    Signed-off-by: Nishant Poorswani <npoorswa@andrew.cmu.edu>

--- code/src/cacheSim_graph_plotter.py
+++ code/src/cacheSim_graph_plotter.py
@@ -1,37 +1,39 @@
-import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np 
 import pandas as pd
+#matplotlib.use('GTK3Agg')
+import matplotlib.pyplot as plt
+
 # Initialize the lists for X and Y
 data = pd.read_csv('output.csv')
   
 df = pd.DataFrame(data)
   
-x = list(df.iloc[:, 0])
-y = list(df.iloc[:, 1])
-z = list(df.iloc[:, 2])
-w = list(df.iloc[:, 3])
-print(y)
-plt.bar(x, y)
+id = list(df.iloc[:, 0])
+hits = list(df.iloc[:, 1])
+misses = list(df.iloc[:, 2])
+eviction = list(df.iloc[:, 3])
 
-plt.title('Simple Example')
-plt.xlabel('Width Names')
-plt.ylabel('Height Values')
-
-plt.show()
-
-plt.bar(x, z)
+plt.bar(id, hits)
 plt.title('Cache Performance: Number of hits')
-plt.xlabel('Cache number')
+plt.xlabel('Cache ID')
 plt.ylabel('Number of hits')
+
 plt.show()
 
-X_axis = np.arange(len(x))
+plt.bar(id, misses)
+plt.title('Cache Performance: Number of misses')
+plt.xlabel('Cache ID')
+plt.ylabel('Number of misses')
+plt.show()
 
-plt.bar(X_axis - 0.3, y, 0.2, label = 'Misses')
-plt.bar(X_axis - 0.1, z, 0.2, label = 'Hits')
-plt.bar(X_axis + 0.1, w, 0.2, label = 'Evictions')
-plt.xticks(X_axis, x)
-plt.xlabel("Cache number")
+X_axis = np.arange(len(id))
+
+plt.bar(X_axis - 0.3, hits, 0.2, label = 'Hits')
+plt.bar(X_axis - 0.1, misses, 0.2, label = 'Misses')
+plt.bar(X_axis + 0.1, eviction, 0.2, label = 'Evictions')
+plt.xticks(X_axis, id)
+plt.xlabel("Cache ID")
 plt.ylabel("Cache hits/misses/evictions")
 plt.title("Cache performance")
 plt.legend()
